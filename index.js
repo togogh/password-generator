eb87ae6c158b61f5fe51b9e669adb308c5c22aa7
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2024-12-30 14:48:00 +0800

    make constants global

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ let symbolsCheckbox = document.getElementById("includeSymbols");
 let passwordOne = document.getElementById("passwordOne");
 let passwordTwo = document.getElementById("passwordTwo");
 
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
 function generatePassword(characters) {
     password = ""
     for (let i=0; i<slider.value; i++) {
@@ -22,10 +26,6 @@ function generatePassword(characters) {
 }
 
 function generatePasswords() {
-    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-    
     let characters = letters
     if (numbersCheckbox.checked) {
         characters = characters.concat(numbers)
