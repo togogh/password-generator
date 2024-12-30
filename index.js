9cc2db6fa0ba7a6a3faf84e1847b54df9d042e39
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2024-12-30 14:39:58 +0800

    add files

---
+++ index.js
@@ -0,0 +1,53 @@
+let slider = document.getElementById("passwordLengthSlider");
+let output = document.getElementById("passwordLengthDisplay");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+let numbersCheckbox = document.getElementById("includeNumbers");
+let symbolsCheckbox = document.getElementById("includeSymbols");
+
+let passwordOne = document.getElementById("passwordOne");
+let passwordTwo = document.getElementById("passwordTwo");
+
+function generatePassword(characters) {
+    password = ""
+    for (let i=0; i<slider.value; i++) {
+        randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+    return password
+}
+
+function generatePasswords() {
+    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+    
+    let characters = letters
+    if (numbersCheckbox.checked) {
+        characters = characters.concat(numbers)
+    }
+    if (symbolsCheckbox.checked) {
+        characters = characters.concat(symbols)
+    }
+    passwordOne.textContent = generatePassword(characters)
+    passwordTwo.textContent = generatePassword(characters)
+}
+
+let message = document.getElementById("message");
+
+function copyToClipboard(num) {
+    if (num === '1') {
+        passwordText = passwordOne.textContent
+    } else {
+        passwordText = passwordTwo.textContent
+    }
+    if (passwordText.length === 0) {
+        message.textContent = "No passwords generated yet."
+    } else {
+        window.prompt("Copy to clipboard: Ctrl+C Enter (or CMD+C Enter on Mac)", passwordText);
+    }
+}
